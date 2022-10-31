8d16cebd052a2a26b3c3768301af69b85083dc5a
OMG0121 <106796509+OMG0121@users.noreply.github.com>
2022-10-31 20:20:19 +0530

    Checkpoint 3
    Added dynamic routes

--- server.js
+++ server.js
@@ -1,24 +1,7 @@
-//This file is entry point of node application.
-//We will be handling movies related WEB API end pnd points here using http module
-//Later on for maintainance purpose; 
-//we will be moving these entry points to routes folder using express
-
-
-//Here we need to handle WEB API End points initially
-
-//---  GET /movies
-//---  GET /genres
-//---  GET /artists
-
-//We also will have to work with URL parameters; but we will deal 
-//with those parameters directly using Express 
 
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    console.log(`method: ${request.method}`);
-    console.log(`url: ${request.url}`);
-
     if (request.url == '/movies') {
         if (request.method == 'GET') {
             response.write("All Movies Data in JSON format from Mongo DB");
@@ -36,6 +19,7 @@ const server = http.createServer((request, response) => {
     response.end();
 });
 
-server.listen(3000, () => {
-    console.log('server started on port 3000');
+
+server.listen(9000, () => {
+    console.log('server started on port 9000');
 });
